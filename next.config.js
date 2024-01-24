0e957a8140684b18aec3f62bd3d0d7639ae32362
Tvrtko <tvrtkobabic2@gmail.com>
2024-01-24 09:06:43 +0100

    rm base path

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-// const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   distDir: "dist",
-  // basePath: isProd ? "/giftshopbjelovar.com" : "",
   trailingSlash: false,
 };
 
